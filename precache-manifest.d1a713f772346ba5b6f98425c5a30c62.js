840b5cb51deb68bcdb8e69c5bd11e6884d5cadea
L. Lester Johnson <shuai.jan28@gmail.com>
2020-02-14 14:14:07 -0500

    Updates

---
+++ precache-manifest.d1a713f772346ba5b6f98425c5a30c62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27da9fe0b1651ec4465979627383ebd",
+    "url": "/robotsapi/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robotsapi/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c0eba9b5ec9a0dd19807",
+    "url": "/robotsapi/static/css/main.e7a7fd62.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robotsapi/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotsapi/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0eba9b5ec9a0dd19807",
+    "url": "/robotsapi/static/js/main.e89bd4e8.chunk.js"
+  },
+  {
+    "revision": "a59c18efe74fb9bc399e",
+    "url": "/robotsapi/static/js/runtime-main.1fb61d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotsapi/static/media/SEGA.12a3db8b.woff"
+  }
+]);
